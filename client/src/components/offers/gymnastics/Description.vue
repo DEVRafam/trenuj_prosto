dd809664b415f46ff7080ffc2072a7610a96e060
DEVRafam <jodax95@gmail.com>
2021-02-04 16:38:04 +0100

    CLIENT gymnastics text added

---
+++ client/src/components/offers/gymnastics/Description.vue
@@ -0,0 +1,9 @@
+<template>
+    <section id="description">
+        <p>
+            Zajęcia odbywają się pod stałym kierunkiem wyspecjalizowanych instruktorów – absolwentów Akademii Wychowania Fizycznego w Krakowie ze specjalizacją Gimnastyka Korekcyjna. Nasi Ambasadorzy mają doskonałe podejście do dzieci i młodzieży, dbają o bezpieczeństwo, uczą
+            zasad Fair Play oraz zachęcają do aktywnego uczestnictwa w spotkaniach.
+        </p>
+        <p>Zależy Ci na harmonijnym rozwoju dziecka? Chcesz uatrakcyjnić ofertę przedszkola? ZAPROŚ NAS NA BEZPŁATNE ZAJĘCIA POKAZOWE !!! Profesjonalna praca u podstaw pozwoli uniknąć deficytów ruchowych w przyszłości.</p>
+    </section>
+</template>
--- client/src/components/offers/gymnastics/Gymnastics_MAIN.vue
+++ client/src/components/offers/gymnastics/Gymnastics_MAIN.vue
@@ -2,16 +2,17 @@
     <section id="gymnastics">
         <OffersHeader></OffersHeader>
         <!--  -->
-        <h1>Zaufały nam następujące przedszkola:</h1>
-        <Kindergartens></Kindergartens>
         <YTVideo></YTVideo>
+        <Description></Description>
+        <Kindergartens></Kindergartens>
         <!--  -->
     </section>
 </template>
 <script>
 import Kindergartens from "./Kindergartens";
 import YTVideo from "./YTVideo";
+import Description from "./Description";
 export default {
-    components: { Kindergartens, YTVideo }
+    components: { Kindergartens, YTVideo, Description }
 };
 </script>
